6684e4b64fb1a271d961e7cbdd0f0b34e9dacd99
Panuwat Khannalae <panumataeh@gmail.com>
2022-09-06 23:03:46 +0700

    - เปลี่ยนสี bg ทุก10 วิ

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const dateText = document.getElementById('date')
 const timeText = document.getElementById('time')
-
+const bgColor = document.getElementById('bg')
+const bgTime = 10
 
 setInterval(() => {
     const day = dayjs().format('ddd D MMM YYYY').toUpperCase()
@@ -8,3 +9,11 @@ setInterval(() => {
     dateText.innerHTML = day
     timeText.innerHTML = time
 }, 1000)
+
+
+setInterval(() => {
+    const red = Math.round(Math.random() * 255)
+    const green = Math.round(Math.random() * 255)
+    const blue = Math.round(Math.random() * 255)
+    bgColor.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`
+}, bgTime * 1000);
